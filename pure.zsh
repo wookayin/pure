e2da082a6e789024fab56eebecff5debd5316d0d
Jongwook Choi <wookayin@gmail.com>
2017-03-19 13:42:09 -0700

    Use absolute path for displaying CWD

--- pure.zsh
+++ pure.zsh
@@ -130,7 +130,7 @@ prompt_pure_preprompt_render() {
 	preprompt+=$prompt_pure_username
 
 	# path
-	preprompt+="%B%F{red}%~%f%b "
+	preprompt+="%B%F{red}%/%f%b "
 	# git info (branch, etc.)
 	preprompt+="%F{$git_color}${vcs_info_msg_0_}%f "
 	# other information
